70b0bb68dc0a8c190fcfd6dd87e5bd576ed55991
Chris <smithchrisgraphics@gmail.com>
2020-12-30 17:15:17 -0500

    Reduced num of files
    Transferred code from vanish.js to Mechanics.js for sake of reducing num of files needed to load for site to work properly

--- js/Mechanics.js
+++ js/Mechanics.js
@@ -13,3 +13,29 @@ $(document).ready(function(){
     });
 });
 
+// Handle page scrolling effect on load
+$(document).ready(function(){
+	
+		// Whenever the user scrolls, trigger this function call
+		$(window).bind('scroll', function() {	
+			
+  			// This value measures how far from the top of the page we are visually
+		   	var distance = 500;
+			
+             if ($(window).scrollTop() > distance) {
+                 $('nav').fadeIn(500);
+				 $('aside').fadeIn(500);
+				
+             }
+             else 
+			 {
+                 $('nav').fadeOut(500);
+				 $('aside').fadeOut(500);
+             }
+		   	
+		     if ($(window).scrollTop() > distance) {
+                 $('header').fadeOut(500);				 
+             }           
+});
+});
+
