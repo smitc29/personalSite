b0dba38768e8dece58c94ec5279999c5fb08203c
Chris <smithchrisgraphics@gmail.com>
2020-12-30 19:41:21 -0500

    Reloading page works better
    Added cookie that detects if someone is returning to the site, immediately tries to skip animations and displays all data asap

--- js/Mechanics.js
+++ js/Mechanics.js
@@ -1,5 +1,5 @@
-var i = 0;
-var txt = " ";
+/*eslint-env browser*/
+/* eslint-disable no-console */ //Enables console output
 
 $(document).ready(function(){
     $(".flipMenu").click(function(){
@@ -13,13 +13,21 @@ $(document).ready(function(){
     });
 });
 
-// Handle page scrolling effect on load
+// These functions are prepped as soon as the site loads initially
 $(document).ready(function(){
 	
-		// Whenever the user scrolls, trigger this function call
+		// Whenever the user scrolls on the page, trigger this function call
 		$(window).bind('scroll', function() {	
 			
-  			// This value measures how far from the top of the page we are visually
+			scrollChecker();
+});
+});
+
+
+/* Function examines where viewer is on page, adjusts visibility of desktop nav and accordingly */
+function scrollChecker()
+{
+	// This value measures how far from the top of the page we are visually
 		   	var distance = 500;
 			
              if ($(window).scrollTop() > distance) {
@@ -36,6 +44,50 @@ $(document).ready(function(){
 		     if ($(window).scrollTop() > distance) {
                  $('header').fadeOut(500);				 
              }           
-});
-});
+}
+// End function scrollChecker()
 
+/* Adds a cookie to the site. Normally triggers 2 seconds after page loads */
+function addCookieToPage()
+{
+	document.cookie = "returning=true";
+	console.log("A cookie has been created for this page.");
+}
+// End function addCookieToPage()
+
+/* On page load, examines if cookie exists for page; if none exist, make one */
+function checkIfCookiePresent()
+{
+	// Obtain site cookie and turn its properties into array
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var save = decodedCookie.split(';');
+	console.log(save);
+	
+	// If there's a cookie from this page before that meets the expected criteria...
+	if(save[0]=='returning=true')
+	{
+		// Bypass intro fading sequence, and immediately display name and starting links
+		console.log("Bypassing intro animation");
+		document.getElementsByTagName("Header")[0].style.display = 'None';
+		scrollChecker();
+		document.getElementById("Chris01").style.animationDuration = "0s";
+		document.getElementById("Smith01").style.animationDuration = "0s";
+		document.getElementsByClassName("ChrisName")[0].classList.add("disableAnimations");
+		document.getElementsByClassName("SmithName")[0].classList.add("disableAnimations");
+		
+		// Displays 5 main links directly below title area on reload
+		var mainLinks = document.getElementsByTagName("a");
+		for(var i = 0; i < mainLinks.length; i++)
+		{
+			mainLinks[i].style.animationDuration = "0s";
+			mainLinks[i].style.animationDelay = "0s";
+		}
+	}
+	// Otherwise, create a cookie after a 2 second delay
+	else
+	{
+		setTimeout(addCookieToPage, 2000); // try again in 2 seconds
+	}
+		
+}
+// End function checkIfCookiePresent()
