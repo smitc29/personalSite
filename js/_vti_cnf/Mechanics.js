d7f96861c10867d8dba45bd335a3662f229ecae9
Chris <smithchrisgraphics@gmail.com>
2021-01-08 20:49:50 -0500

    lastSafeVersion looking better!
    lastSafeVersion is now actually a stable site; will keep on record for future records

---
+++ js/_vti_cnf/Mechanics.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Sep 2018 02:24:41 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|contact.html newHome.html indexWithContact.html maingallery.html contact_backup.html dropdowns.html gallery_backup.html index\\ -\\ Copy.html index.html
